580bdb40235aac849b3d89ae09af4aac89d159d9
Rashang Soni <rashangsoni@gmail.com>
2023-04-14 10:52:20 +0100

    java_week_5_writing_hw_rashangsoni

---
+++ src/java_week_5_hw_rashangsoni/Programme_4_ColoursArrayList.java
@@ -0,0 +1,30 @@
+package java_week_5_hw_rashangsoni;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Jay Vaghani
+ */
+/*
+Write a Java program to create a new array list, add some colours (string)
+and printout the collection using for each loop.
+ */
+public class Programme_4_ColoursArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> colorList = new ArrayList<>();
+        colorList.add("RED");
+        colorList.add("BLUE");
+        colorList.add("GREEN");
+        colorList.add("YELLOW");
+        colorList.add("BLACK");
+        colorList.add("WHITE");
+        colorList.add("ORANGE");
+        colorList.add("PURPLE");
+        colorList.add("CYAN");
+
+        for (String colourList : colorList) {
+            System.out.print(colourList + ", ");
+        }
+    }
+}
+
